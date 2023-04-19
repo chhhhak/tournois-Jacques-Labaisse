46511372f64109be44238c2561c7a41445bdab8a
jlabaisse <jlabaisse@laas.fr>
2023-04-19 09:09:02 +0200

    create poule html template and correct poule view

--- tournois/tournaments/views.py
+++ tournois/tournaments/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Poule, Tournoi
 
 def poule(request, id_poule):
-    poule = get_object_or_404(Poule, pk=id_poule)
-    return render(request, 'tournois/poule.html', {'poule', poule})
+    pool = get_object_or_404(Poule, pk=id_poule)
+    return render(request, 'tournois/poule.html', {'pool': pool})
 
 def tournois(request):
     tournament_list = get_list_or_404(Tournoi)
