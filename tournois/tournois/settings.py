6f59fef54ce97459f3accea1235b11d942bcf688
jlabaisse <jlabaisse@laas.fr>
2023-04-20 19:00:28 +0200

    add linking for login formular

--- tournois/tournois/settings.py
+++ tournois/tournois/settings.py
@@ -100,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Redirection after (dis)connection
+LOGIN_REDIRECT_URL = "../../tournois"
+LOGOUT_REDIRECT_URL = "login"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- tournois/tournois/urls.py
+++ tournois/tournois/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('tournaments.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
