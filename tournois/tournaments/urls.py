33eeb28aaba55206490a71b804c1ae2f433a5e7c
jlabaisse <jlabaisse@laas.fr>
2023-04-20 16:02:43 +0200

    add match view and template

--- tournois/tournaments/urls.py
+++ tournois/tournaments/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "tournaments"
 urlpatterns = [
+    path('match/<int:id_match>', views.match, name='match'),
     path('poule/<int:id_poule>', views.poule, name='poule'),
     path('tournois', views.tournois, name = 'tournois'),
     path('tournoi/<int:id_tournoi>', views.tournoi, name = 'tournoi')
--- tournois/tournaments/views.py
+++ tournois/tournaments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 
-from .models import Equipe, Poule, Tournoi
+from .models import Equipe, Match, Poule, Tournoi
 
 def poule(request, id_poule):
     pool = get_object_or_404(Poule, pk=id_poule)
@@ -15,3 +15,7 @@ def tournois(request):
 def tournoi(request, id_tournoi):
     tournament = get_object_or_404(Tournoi, pk=id_tournoi)
     return render(request, 'tournois/tournoi.html', {'tournament': tournament})
+
+def match(request, id_match):
+    match = get_object_or_404(Match, pk=id_match)
+    return render(request, 'tournois/match.html', {'match': match} )
